4500aa77879c1895acd15e2519a17e3dc4da1352
Pranshu Garg <gargpranshuwork92@gmail.com>
2026-04-07 20:23:04 +0530

    070426

--- App.js
+++ App.js
@@ -1,18 +1,72 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+//  React Elements
+// React element using React
+// const heading = React.createElement("h1", {id: "head"}, "Nameste React");
+// console.log(heading);
 
-// JSX
+// JSX (in React) - Not a HTML syntax (it is simillar to HTML)
+// React and JSX are 2 different things
 
-console.log(parent); // object
+//  React element using JSX
+// JSX code got transpiled for JSEngine
+// const JSXHeading = <h1 className="head" tabIndex="1">Nameste React using JSX🚀</h1>;
+// for multiple lines -- ()
+// const JSXHeading = (<h1 className="head" tabIndex="1">
+//  Nameste React using JSX🚀
+//  </h1>);
+
+// React Components -
+//  Functional Component
+//  1.
+// const HeadingComponent = () =>{
+//   return <h1>Namaste React Functional Component of React</h1>
+// };
+
+// 2.
+// const HeadingComponent2 = () =>(
+//   <div>
+//     <h1>Namaste React Functional Component of React</h1>
+//   </div>
+// );
+
+// 3.
+// const HeadingComponent3 = () => <h1>Namaste React Functional Component of React</h1>;
+//  1 , 2 and 3 are same
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(JSXHeading);
+
+// to render a functional component
+// root.render(<HeadingComponent2 />)
+
+const number = 9000
+
+const Title = function () {
+  return(<h1 className="heading">
+    Its over {number} by the way,
+    Namaste React from functional component
+  </h1>);
+};
+
+const jsVari = (
+  <div>
+    <h1>
+      It is just a simple React Elements
+      which is 1st ly converted to Object and then converted into HTML element
+    </h1>
+  </div>
+)
+
+const HeadingComponent = () => (
+  <div id="container">
+    {number} its not just a number , by the way
+    <Title />
+    {jsVari}
+    {console.log("Its over 9000 is the phrase given by vegita")}
+    <h1>Namaste React from fun comp</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
